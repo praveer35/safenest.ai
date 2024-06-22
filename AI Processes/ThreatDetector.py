3155e5e1cd532986360c8320b5f14341d0ebba6c
Samuel Araya <saraya95@yahoo.com>
2024-06-22 16:55:06 -0700

    Implemented Claude 3.5 to evaluate enviromental threats in a json format

--- AI Processes/ThreatDetector.py
+++ AI Processes/ThreatDetector.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 from botocore.exceptions import ClientError
 
 MODEL_ID = "anthropic.claude-3-5-sonnet-20240620-v1:0"
@@ -10,14 +11,40 @@ bedrock_runtime = boto3.client("bedrock-runtime", region_name="us-east-1")
 with open(IMAGE_NAME, "rb") as f:
     image = f.read()
 
-user_message = "Is this baby in danger? Give a danger level between 1-5. Output in JSON."
+#user_message = "Is this baby in danger? Give a danger level between 1-5. Output in JSON."
+
+
+ml_prompt = f"""
+    Examine this picture and check for dangerous enviromental factors
+    Evaulate each of the dangerous enviormental factors on their threat to the baby
+    Give a score between 1-10 representing the probability of the baby sustaining an injury from the enviroment
+    Also make the output sound as if you're talking to the baby's parents.
+    For "Enviromental_Factors" we want to generate one for each of the dangerous factors found in the picture. 
+    Answer strictly in the following JSON format. Do not change the keys. Just return the JSON string.
+    - {json.dumps({
+    "risk_probability": "Probability of the baby sustaining an injury from the enviroment (value from 1 to 10)",
+    "assessment":"Basic assessment given the probability and the baby's enviroment. Should recommend to fix",
+    "Enviromental_Factors" : [ {
+        "Factor_Name": "This is the name of the enviromental factor",
+        "Threat_Summary": "summary of the danger this factor poses to the baby",
+        "Preventitaive action": [
+            {
+            "task": "name of the task",
+            "task_description": "detailed description of the action to be taken to eliminate or reduce threat of the enviromental factor",
+            },
+            "..."
+        ]
+    }, "..."
+    ]
+})} 
+    """
 
 messages = [
     {
         "role": "user",
         "content": [
             {"image": {"format": "png", "source": {"bytes": image}}},
-            {"text": user_message},
+            {"text": ml_prompt},
         ],
     }
 ]
@@ -28,3 +55,5 @@ response = bedrock_runtime.converse(
 )
 response_text = response["output"]["message"]["content"][0]["text"]
 print(response_text)
+
+
