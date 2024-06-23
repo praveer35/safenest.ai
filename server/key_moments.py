6dd401af3676a4d6ba67a048e5eb6513e9629ae4
Amogh Jani <amoghjani@wifi-10-40-129-77.wifi.berkeley.edu>
2024-06-22 17:49:37 -0700

    changed default parameters to reduce frames

--- server/key_moments.py
+++ server/key_moments.py
@@ -5,7 +5,7 @@ import argparse
 # Supported video file extensions
 SUPPORTED_EXTENSIONS = {'.mp4', '.avi', '.mov', '.mkv', '.wmv', '.flv'}
 
-def extract_key_frames(video_path, threshold=30, min_contour_area=500):
+def extract_key_frames(video_path, threshold=100, min_contour_area=1000):
     # Check file extension
     _, ext = os.path.splitext(video_path)
     if ext.lower() not in SUPPORTED_EXTENSIONS:
@@ -81,8 +81,8 @@ def extract_key_frames(video_path, threshold=30, min_contour_area=500):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract key frames from a video.')
     parser.add_argument('video_path', type=str, help='Path to the video file')
-    parser.add_argument('--threshold', type=int, default=30, help='Threshold for detecting motion')
-    parser.add_argument('--min_contour_area', type=int, default=500, help='Minimum contour area to be considered as motion')
+    parser.add_argument('--threshold', type=int, default=100, help='Threshold for detecting motion')
+    parser.add_argument('--min_contour_area', type=int, default=1000, help='Minimum contour area to be considered as motion')
     args = parser.parse_args()
 
     extract_key_frames(args.video_path, args.threshold, args.min_contour_area)
